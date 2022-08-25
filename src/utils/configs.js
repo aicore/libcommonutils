5341eaa926ad445570c99fefe354ab9dd47c39f7
Charly Abraham <charly@core.ai>
2022-08-25 19:44:21 +0530

    docs: update docs

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,9 +1,25 @@
 import * as crypto from "crypto";
 
 /**
- * This function helps to get configurations for mySQL DB
+ * @typedef {Object} MySqlConfigs
+ * @property {string} host - hostname or ip address
+ * @property {string} port - port on which MySql server is listening
+ * @property {string} database - name of database to do operations
+ * @property {string} user - name of user who has permissions to do operation
+ * @property {string} password - password of the user for authentication
  *
  */
+
+/**
+ * It returns a JavaScript object with the host, port, database, user, and password for a MySQL database
+ * @returns  {MySqlConfigs} An object with the following properties:
+ *     host: The value of the environment variable MY_SQL_SERVER or 'localhost'
+ *     port: The value of the environment variable MY_SQL_SERVER_PORT or '3306'
+ *     database: The value of the environment variable MY_SQL_SERVER_DB or a random hex string
+ *     user: The value of the environment variable MY_SQL_USER or a random hex string
+ *     password :  The value of the environment variable MY_SQL_PASSWORD or a random hex string
+ */
+
 export function getMySqlConfigs() {
     const host = process.env.MY_SQL_SERVER || 'localhost';
     const port = process.env.MY_SQL_SERVER_PORT || '3306';
